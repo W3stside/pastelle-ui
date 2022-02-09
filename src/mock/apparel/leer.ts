5b200e19a9e76e181d29790b4afa544eefa665e5
David W3stside <david.sato64@gmail.com>
2022-02-09 00:00:41 -0300

    loq qual posters

--- src/mock/apparel/leer.ts
+++ src/mock/apparel/leer.ts
@@ -5,8 +5,8 @@ import FRONT_IMAGE_SMALL from 'assets/apparel/leer/images/front-small.jpg'
 import BACK_IMAGE_SMALL from 'assets/apparel/leer/images/back-small.jpg'
 import FRONT_VIDEO from 'assets/apparel/leer/video/front.webm'
 import BACK_VIDEO from 'assets/apparel/leer/video/back.webm'
-import FRONT_VIDEO_POSTER from 'assets/apparel/leer/video/front-poster.png'
-import BACK_VIDEO_POSTER from 'assets/apparel/leer/video/back-poster.png'
+import FRONT_VIDEO_POSTER from 'assets/apparel/leer/video/front-poster-lq.jpg'
+import BACK_VIDEO_POSTER from 'assets/apparel/leer/video/back-poster-lq.jpg'
 import { ApparelItem, HomeItem } from './types'
 import { SocialType } from 'pages/SingleItem/AsideWithVideo'
 
@@ -28,7 +28,7 @@ E io ch’avea d’error la testa cinta,
 dissi: Maestro, che è quel ch’i’ odo?
 e che gent’ è che par nel duol sì vinta?
 
-Big shout to Matheiu Sato in Bordeaux for the painting on the front.
+Big shout to Mathieu Sato in Bordeaux for the painting on the front.
 @mathieusato
 `
 
