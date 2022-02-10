cf5ecd7a419bfbfe13066d7f0469065e3446512b
David W3stside <david.sato64@gmail.com>
2022-02-09 21:47:17 -0300

    add new shirt content

--- src/mock/apparel/index.ts
+++ src/mock/apparel/index.ts
@@ -1,5 +1,6 @@
 import { HomeItemsList } from './types'
 // APPAREL DATA
 import LEER from 'mock/apparel/leer'
+import ESTRELA from 'mock/apparel/estrela'
 
-export default [LEER] as HomeItemsList
+export default [LEER, ESTRELA] as HomeItemsList
