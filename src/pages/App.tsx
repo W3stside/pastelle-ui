56b073dc5912ebd837a17c6e7116ab9e550037b9
virgil-pastelle <doohhello@gmail.com>
2022-02-18 12:00:14 -0300

    url to component in App pages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom'
 
 import ThemeViewer from 'components/ThemeViewer'
 import Popups from 'components/Popups'
@@ -8,8 +8,24 @@ import Web3ReactManager from 'components/blockchain/Web3ReactManager'
 import Header from 'components/Header'
 // import Footer from 'components/Footer'
 
-import Home from 'pages/Home'
+import Catalog from 'pages/Catalog'
 import Navigation from 'components/Navigation'
+
+// TODO: move
+// Redirects to swap but only replace the pathname
+const DEFAULT_CATALOG_YEAR = '2022'
+const DEFAULT_CATALOG_SEASON = 'FALL'
+const DEFAULT_CATALOG_START_ITEM = 'LEER'
+export function RedirectPathToCatalogOnly({ location }: RouteComponentProps) {
+  return (
+    <Redirect
+      to={{
+        ...location,
+        pathname: `catalog/${DEFAULT_CATALOG_YEAR}/${DEFAULT_CATALOG_SEASON}/${DEFAULT_CATALOG_START_ITEM}`
+      }}
+    />
+  )
+}
 
 export default function App() {
   return (
@@ -22,8 +38,9 @@ export default function App() {
         <Navigation />
         {/* ARTICLE CONTENT */}
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact strict path="/catalog/:year/:season/:itemName" component={Catalog} />
           <Route exact path="/theme" component={ThemeViewer} />
+          <Route component={RedirectPathToCatalogOnly} />
         </Switch>
         {/* FOOTER */}
         {/* <Footer /> */}
