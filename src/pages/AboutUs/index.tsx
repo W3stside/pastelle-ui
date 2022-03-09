71363eb39c55098681fc46e9beee2f5e6c3104f0
virgil-pastelle <doohhello@gmail.com>
2022-03-09 17:24:27 +0000

    add markdown support

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -1,13 +1,19 @@
 import styled from 'styled-components/macro'
+
 import { AutoColumn, Row } from 'components/Layout'
+import { MarkdownRenderer } from 'components/Markdown'
+
 import { CatalogContainer } from 'pages/Catalog'
 import { ItemAsidePanel, ItemContainer, ItemHeader } from 'pages/SingleItem/styleds'
+
 import { TYPE } from 'theme'
 import { ThemeModes } from 'theme/styled'
 import { getThemeColours } from 'theme/utils'
-
+// assets
 import david from 'assets/images/aboutus/david.jpeg'
 import felix_david from 'assets/images/aboutus/felix_david_train_cropped.jpg'
+// markdown
+import AboutUsContent from 'markdown/AboutUs.md'
 
 const AboutUsContainer = styled(ItemContainer)`
   > ${ItemAsidePanel} {
@@ -51,21 +57,7 @@ export default function AboutUs() {
           </AboutUsHeader>
           <AboutUsTextContainer>
             <TYPE.black fontSize={18} padding={2} fontWeight={300}>
-              Mentally established at the peak of fidget spinners, and legally established in 2022, Pastelle is
-              headquarted in Lisboa, with Portugal standing firmly as the base of operations for the complete lifecycle
-              of our streetwear.
-              <br />
-              <br />
-              The goal here is pretty simple. We make, and want to continue to make, high quality apparel that not only
-              feels solid, but embodies a unique and wholly bespoke feel that sets us apart from the noise. Design
-              starts here in LX, then production and logistics is handled up North in Porto. Somewhere in between
-              we&apos;re driving up and down to get shirts out and into your hands.
-              <br />
-              <br />
-              Oh and a note on designs - we collab with smaller, local artists and work together to make every design.
-              We know more than anyone that you&apos;re not trying to wear the same shit the next fourteen dudes all
-              already wore... so we&apos;re already built out to integrate NFTs with every purchase. Now you can swag
-              out and we&apos;ll kit you out with the authenticity to boot.
+              <MarkdownRenderer contentFile={AboutUsContent} />
             </TYPE.black>
           </AboutUsTextContainer>
         </ItemAsidePanel>
