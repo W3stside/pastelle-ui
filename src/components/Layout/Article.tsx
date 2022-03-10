6d36c8cf1e596c2f592f3e859bd67c920aa8e6ec
virgil-pastelle <doohhello@gmail.com>
2022-03-10 20:44:29 +0000

    add new bg

--- src/components/Layout/Article.tsx
+++ src/components/Layout/Article.tsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components/macro'
 import { fadeInAnimation } from 'pages/SingleItem/styleds'
+import portugalBg from 'assets/images/portugal-bg.jpg'
 
 export const ArticleFadeInContainer = styled.article`
   overflow: hidden;
   filter: contrast(1) blur(0px);
+
+  background-image: url(${portugalBg});
+  background-size: contain;
 
   ${fadeInAnimation};
 
