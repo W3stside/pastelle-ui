311f6933d51743da857ea2259c5a9b24f15b0669
virgil-pastelle <doohhello@gmail.com>
2022-03-16 15:54:09 +0000

    fix bg pic url

--- src/components/Layout/Article.tsx
+++ src/components/Layout/Article.tsx
@@ -3,7 +3,7 @@ import { fadeInAnimation } from 'pages/SingleItem/styleds'
 
 const portugalBg =
   process.env.REACT_APP_IMAGEKIT_URL_ENDPOINT +
-  'portugal-bg_Rqj8jTKhFmds.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1647443573520'
+  '/portugal-bg_Rqj8jTKhFmds.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1647443573520'
 
 export const ArticleFadeInContainer = styled.article`
   overflow: hidden;
