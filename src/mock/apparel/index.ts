227f79385b94f1c0d80d2754eb4d3cfaebc784bb
virgil-pastelle <doohhello@gmail.com>
2022-03-16 09:37:01 +0000

    simplify

--- src/mock/apparel/index.ts
+++ src/mock/apparel/index.ts
@@ -1,16 +1,16 @@
-import { CatalogSeasonItemMap, CatalogSeasonsMap } from './types'
+import { /* CatalogSeasonItemMap, */ CatalogSeasonsMap } from './types'
 
 import * as FALL_CATALOG from 'mock/apparel/2022/FALL'
 
-const CATALOG = Object.entries(FALL_CATALOG).reduce((acc, [itemName, itemContent]) => {
+/* const CATALOG = Object.entries(FALL_CATALOG).reduce((acc, [itemName, itemContent]) => {
   acc[itemName] = itemContent
   return acc
-}, {} as CatalogSeasonItemMap)
+}, {} as CatalogSeasonItemMap) */
 
 const catalogItems: Map<number | string, Partial<CatalogSeasonsMap>> = new Map()
 
 catalogItems.set('2022', {
-  FALL: CATALOG
+  FALL: FALL_CATALOG
 })
 
 export default catalogItems
