5301c07a1e8ccb8ac1016b17a27d7cc4e3da9904
virgil-pastelle <doohhello@gmail.com>
2022-03-19 19:46:56 +0000

    add configs

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,8 +1,11 @@
-import { ItemSizes } from 'mock/apparel/types'
+import { CatalogSeason, ItemSizes } from 'mock/apparel/types'
 
+export const CURRENT_YEAR = new Date().getFullYear()
+export const CURRENT_SEASON: CatalogSeason = 'FALL'
+
+export const SEASONS: CatalogSeason[] = ['WINTER', 'SPRING', 'SUMMER', 'FALL']
 export const STORE_IMAGE_SIZES = {
   LARGE: 2000,
   SMALL: 500
 }
-
 export const SHIRT_SIZES: ItemSizes[] = ['X-LARGE', 'LARGE', 'MEDIUM', 'SMALL']
