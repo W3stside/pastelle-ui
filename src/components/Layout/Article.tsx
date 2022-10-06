ca41942742d2b2c20e91c57042ded0691110789c
virgil-pastelle <doohhello@gmail.com>
2022-10-05 19:36:42 -0500

    remove comment

--- src/components/Layout/Article.tsx
+++ src/components/Layout/Article.tsx
@@ -23,36 +23,3 @@ export const ArticleFadeInContainer = styled.article`
   filter: contrast(1) blur(0px);
   ${setFadeInAnimation()}
 `
-/* 
-const BG_RATIO = 2.182
-const BG_HEIGHT_RATIOS = {
-  extraSmall: MEDIA_WIDTHS.upToExtraSmall / BG_RATIO,
-  small: MEDIA_WIDTHS.upToSmall / BG_RATIO,
-  medium: MEDIA_WIDTHS.upToMedium / BG_RATIO,
-  large: MEDIA_WIDTHS.upToLarge / BG_RATIO,
-  extraLarge: MEDIA_WIDTHS.upToExtraLarge / BG_RATIO
-}
-// load extra small size
-  background-image: url(${portugalBg},w-${MEDIA_WIDTHS.upToExtraSmall},h-${
-    BG_HEIGHT_RATIOS.extraSmall
-  }), url(${portugalBgLq}:w-${MEDIA_WIDTHS.upToExtraSmall},h-${BG_HEIGHT_RATIOS.extraSmall});
-
-// from small
-  ${fromSmall`
-    background-image: url(${portugalBg},w-${MEDIA_WIDTHS.upToSmall},h-${BG_HEIGHT_RATIOS.small}), url(${portugalBgLq}:w-${MEDIA_WIDTHS.upToSmall},h-${BG_HEIGHT_RATIOS.small});
-  `}
-  // from med
-  ${fromMedium`
-    background-image: url(${portugalBg},w-${MEDIA_WIDTHS.upToMedium},h-${BG_HEIGHT_RATIOS.medium}), url(${portugalBgLq}:w-${MEDIA_WIDTHS.upToMedium},h-${BG_HEIGHT_RATIOS.medium});
-  `}
-  // from large
-  ${fromLarge`
-    background-image: url(${portugalBg},w-${MEDIA_WIDTHS.upToLarge},h-${BG_HEIGHT_RATIOS.large}), url(${portugalBgLq}:w-${MEDIA_WIDTHS.upToLarge},h-${BG_HEIGHT_RATIOS.large});
-  `}
-  // from xl
-  ${fromExtraLarge`
-    background-image: url(${portugalBg},w-${MEDIA_WIDTHS.upToExtraLarge},h-${BG_HEIGHT_RATIOS.extraLarge}), url(${portugalBgLq}:w-${MEDIA_WIDTHS.upToExtraLarge},h-${BG_HEIGHT_RATIOS.extraLarge});
-  `}
-
-  background-size: contain;
-*/
