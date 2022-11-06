d30d1d98e28dba22d5f08c9d957a1d83d2df3e6a
virgil-pastelle <doohhello@gmail.com>
2022-11-06 23:18:57 +0000

    env update

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,2 @@
 export const SHOW_CART = !!JSON.parse(process.env.REACT_APP_USE_CHECKOUT || 'false')
-export const IS_PRE_PROD = !!JSON.parse(process.env.REACT_APP_IS_PRE_PROD || 'false')
+export const IS_PRE_PROD = !!JSON.parse(process.env.REACT_APP_IS_PRE_PROD || 'true')
