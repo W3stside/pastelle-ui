f042aeff47f4dce95a3fd04a01bc72a3428ac430
dave <david.sato64@gmail.com>
2022-12-08 00:15:48 +0000

    Reset sensitivity

--- src/components/Carousel/AnimatedCarousel.tsx
+++ src/components/Carousel/AnimatedCarousel.tsx
@@ -24,7 +24,7 @@ export default function AnimatedCarousel({
     springs,
     state: { currentIndex, width },
     refCallbacks: { setItemSizeRef }
-  } = useLimitedHorizontalScroll(imageList, { sensitivity: 4 })
+  } = useLimitedHorizontalScroll(imageList, { sensitivity: 2 })
 
   return (
     <CarouselContainer
