0d696442200e90e2d668c9455943a9a44f99ee80
virgil-pastelle <doohhello@gmail.com>
2022-12-09 00:48:17 +0000

    cart logic update

--- src/state/cart/updater.tsx
+++ src/state/cart/updater.tsx
@@ -4,28 +4,41 @@ import { useEffect } from 'react'
 import { useCreateCart } from 'shopify/graphql/hooks'
 import { GET_CART } from 'shopify/graphql/queries/cart'
 import { GetCartQuery, GetCartQueryVariables } from 'shopify/graphql/types'
-import { useUpdateCartInfoDispatch, useGetCartIdState } from 'state/cart/hooks'
+import { useUpdateCartInfoDispatch, useGetCartIdState, useToggleCartAndState } from 'state/cart/hooks'
 import { UpdateCartInfoParams } from './reducer'
 
 interface CartCreationCbs {
   createCart: ReturnType<typeof useCreateCart>[0]
   updateCartInfo: (params: UpdateCartInfoParams) => void
+  toggleCartOptions: ReturnType<typeof useToggleCartAndState>
 }
 
 export default function CartUpdater() {
+  const { query } = useApolloClient()
   const [createCart] = useCreateCart()
   const updateCartInfo = useUpdateCartInfoDispatch()
-  const { query } = useApolloClient()
+  const toggleCartOptions = useToggleCartAndState()
 
   const cartId = useGetCartIdState()
 
   useEffect(() => {
-    if (cartId) {
-      _queryAndUpdateCart({ cartId, query, updateCartInfo })
-    } else {
-      _createAndSaveCart({ createCart, updateCartInfo })
+    async function initCart(cartId: string | undefined) {
+      try {
+        await (cartId
+          ? _queryAndUpdateCart({ cartId, query, updateCartInfo })
+          : _createAndSaveCart({ createCart, updateCartInfo, toggleCartOptions }))
+      } catch (error) {
+        if (error?.type === 'EMPTY_CART') {
+          console.debug('[CART UPDATER] --> EMPTY_CART - INITIATING NEW CART!')
+          _createAndSaveCart({ createCart, updateCartInfo, toggleCartOptions })
+        } else {
+          throw error
+        }
+      }
     }
-  }, [cartId, createCart, query, updateCartInfo])
+
+    initCart(cartId)
+  }, [cartId, createCart, query, updateCartInfo, toggleCartOptions])
 
   return null
 }
@@ -42,28 +55,31 @@ function _queryAndUpdateCart({ cartId, query, updateCartInfo }: QueryCartParams)
     variables: { cartId, linesAmount: DEFAULT_CART_LINES_AMOUNT }
   })
     .then(({ data, error }) => {
-      if (error) throw error
+      if (error) throw { type: 'QUERY_ERROR', error }
+      else if (!data?.cart || data?.cart === null) throw { type: 'EMPTY_CART' }
 
-      if (!!data?.cart?.totalQuantity) {
-        console.debug('[CART UPDATER::PREVIOUS CART SUB-UPDATER] --> Previous cart found! Setting.')
-        updateCartInfo({
-          cartId: data.cart.id,
-          totalQuantity: data.cart.totalQuantity,
-          costs: data.cart.cost
-        })
-      }
+      console.debug('[CART UPDATER::PREVIOUS CART SUB-UPDATER] --> Previous cart found! Setting.')
+      updateCartInfo({
+        cartId: data.cart.id,
+        totalQuantity: data.cart.totalQuantity,
+        costs: data.cart.cost
+      })
     })
     .catch(error => {
-      console.error('CART UPDATER::PREVIOUS CART UPDATE ERROR!', error)
+      throw error
     })
 }
 
-async function _createAndSaveCart({ updateCartInfo, createCart }: CartCreationCbs) {
+async function _createAndSaveCart({ updateCartInfo, createCart, toggleCartOptions: [state, setter] }: CartCreationCbs) {
+  state && setter(false)
   return createCart()
     .then(({ data: cartResponse }) => {
       const cart = cartResponse?.cartCreate?.cart
 
-      cart && updateCartInfo({ cartId: cart.id })
+      if (cart) {
+        console.debug('[CART UPDATER::NEW CART SUB-UPDATER] --> New cart initialised!')
+        updateCartInfo({ cartId: cart.id })
+      }
     })
     .catch(error => {
       console.error('[CART UPDATERS] Cart initialisation error!', error)
