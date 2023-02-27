56e73d6b55c4c9220d46395a1ec3b0318f762f12
virgil-pastelle <doohhello@gmail.com>
2023-02-27 18:32:04 +0000

    _redirects file

--- src/pages/SingleProduct/index.tsx
+++ src/pages/SingleProduct/index.tsx
@@ -2,9 +2,7 @@ import { useStateRef } from '@past3lle/hooks'
 import { devDebug } from '@past3lle/utils'
 import SEO from 'components/SEO'
 import { useEffect, useMemo } from 'react'
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
-import { DEFAULT_CURRENT_COLLECTION_VARIABLES, useQueryProductById } from 'shopify/graphql/hooks'
-import { getShopifyId } from 'shopify/utils'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useCurrentCollection, useUpdateCurrentlyViewingProduct } from 'state/collection/hooks'
 
 import AsideWithVideo from './AsideWithVideo'
@@ -13,14 +11,6 @@ import { SingleProductArticle } from './styled'
 export default function SingleItem() {
   const [container, setContainerRef] = useStateRef<HTMLElement | null>(null, (node) => node)
   const parentAspectRatio = getNodeAspectRatio(container)
-
-  const [searchParams] = useSearchParams()
-  // TODO: use this properly
-  useQueryProductById({
-    id: getShopifyId(searchParams.get('skillId'), 'Product'),
-    imageAmt: DEFAULT_CURRENT_COLLECTION_VARIABLES.imageAmt,
-    videoAmt: DEFAULT_CURRENT_COLLECTION_VARIABLES.videoAmt,
-  })
 
   // Internal referrer = shopify handle
   // External referrer = shopfiy ID
@@ -61,3 +51,13 @@ function getNodeAspectRatio(node: HTMLElement | undefined | null) {
   if (!node) return undefined
   return node.clientWidth / node.clientHeight
 }
+
+/* 
+const [searchParams] = useSearchParams()
+  // TODO: use this properly
+  useQueryProductById({
+    id: getShopifyId(searchParams.get('skillId'), 'Product'),
+    imageAmt: DEFAULT_CURRENT_COLLECTION_VARIABLES.imageAmt,
+    videoAmt: DEFAULT_CURRENT_COLLECTION_VARIABLES.videoAmt,
+  })
+*/
