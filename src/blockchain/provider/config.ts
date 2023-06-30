14c7dec31908b59355c7efb3d2e3e2590a64509a
virgil-pastelle <doohhello@gmail.com>
2023-06-30 14:30:52 +0100

    config

--- src/blockchain/provider/config.ts
+++ src/blockchain/provider/config.ts
@@ -1,4 +1,5 @@
-import { ForgeWeb3ModalProps, Web3ModalConfigWeb3Props } from '@past3lle/forge-web3'
+import { ForgeWeb3ModalProps } from '@past3lle/forge-web3'
+import LOGO_512 from 'assets/images/512_logo.png'
 import GOOGLE_APPLE_LOGO from 'assets/images/google-apple.png'
 import { pstlModalTheme as MODAL_THEME, pastelleTheme } from 'theme'
 import { polygon, polygonMumbai } from 'wagmi/chains'
@@ -8,7 +9,7 @@ if (
   !process.env.REACT_APP_WEB3_AUTH_PROJECT_ID ||
   !process.env.REACT_APP_WEB3_AUTH_NETWORK
 ) {
-  throw new Error('Missing W3A and/or W3M projectID env variables!')
+  throw new Error('Missing Web3Auth and/or WalletConnect projectID env variables!')
 }
 export const SUPPORTED_CHAINS_BY_ENV = process.env.NODE_ENV === 'production' ? [polygon] : [polygonMumbai]
 export const WEB3_MODAL_PROPS: ForgeWeb3ModalProps = {
@@ -16,11 +17,12 @@ export const WEB3_MODAL_PROPS: ForgeWeb3ModalProps = {
   chains: SUPPORTED_CHAINS_BY_ENV,
   wagmiClient: {
     options: {
+      autoConnect: true,
       pollingInterval: 30_000,
     },
   },
   modals: {
-    pstl: {
+    root: {
       title: 'LOGIN    PASTELLE.SHOP',
       hideInjectedFromRoot: true,
       closeModalOnConnect: true,
@@ -45,18 +47,23 @@ export const WEB3_MODAL_PROPS: ForgeWeb3ModalProps = {
         },
       },
     },
-    w3a: {
+    web3auth: {
       appName: 'PASTELLE.SHOP',
-      network:
-        process.env.NODE_ENV === 'production'
-          ? (process.env.REACT_APP_WEB3_AUTH_NETWORK as Web3ModalConfigWeb3Props['modals']['w3a']['network'])
-          : 'testnet',
+      network: process.env.REACT_APP_WEB3_AUTH_NETWORK as any,
       projectId: process.env.REACT_APP_WEB3_AUTH_PROJECT_ID,
+      storageKey: 'session',
       preset: 'DISALLOW_EXTERNAL_WALLETS',
       mfaLevel: 'none',
-      uxMode: 'popup',
+      uxMode: 'redirect',
+      themeInfo: {
+        mode: 'dark',
+        primary: pastelleTheme.modes.DEFAULT.purple,
+      },
+      url: 'https://pastelle.shop',
+      appLogoDark: LOGO_512,
+      appLogoLight: LOGO_512,
     },
-    w3m: {
+    walletConnect: {
       projectId: process.env.REACT_APP_WEB3_MODAL_PROJECT_ID,
       themeMode: 'dark',
       themeVariables: {
