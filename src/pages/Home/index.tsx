8d9db289f0a1a0e80897b036deab44a8947d961c
virgil-pastelle <doohhello@gmail.com>
2023-10-17 11:55:30 +0100

    HomePage: updates & opts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,92 +1,38 @@
 import { useIsMobile } from '@past3lle/hooks'
-import { urlToSimpleGenericImageSrcSet } from '@past3lle/theme'
 import SEO from 'components/SEO'
+import { SelectedShowcaseVideo } from 'components/Showcase/Videos'
 import { COLLECTION_PATHNAME } from 'constants/navigation'
-import { SingleAsidePage } from 'pages/common'
+import {
+  SingleProductAsidePanel,
+  SingleProductContainer,
+  SingleProductScreensContainer,
+} from 'pages/SingleProduct/styled'
+import { SinglePageSmartWrapper } from 'pages/common'
 import { useGetCommonPropsFromProduct } from 'pages/common/hooks/useGetCommonPropsFromProduct'
 import { useProductWebCarouselActions } from 'pages/common/hooks/useProductCarouselActions'
-import { BaseProductPageProps } from 'pages/common/types'
-import { useCallback, useMemo } from 'react'
+import { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { FragmentProductImageFragment, FragmentProductVideoFragment } from 'shopify/graphql/types'
-import { useCurrentCollection } from 'state/collection/hooks'
+import { useQueryHomepage } from 'shopify/graphql/hooks'
 import { useThemeManager } from 'state/user/hooks'
 
 import * as Screens from '../common/screens'
 
-const LOGO = 'https://pastelle.shop/static/media/pastelle-ivory-outlined.06d3dadfc9e4e7c2c8904b880bf4067c.svg'
+const VIDEO_CSS = `
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 
-const baseProduct: Omit<
-  BaseProductPageProps,
-  'id' | 'images' | 'videos' | 'headerLogo' | 'navLogo' | 'logo' | 'skillMetadata'
-> = {
-  altColor: 'darkslateblue',
-  bgColor: 'darkslateblue',
-  color: '#626a42',
-  title: 'PASTELLE',
-  handle: 'pastelle',
-  productType: 'string',
-  //   logo: undefined,
-  //   headerLogo: undefined,
-  //   navLogo: undefined,
-  //   images: [],
-  lockedImages: [],
-  sizeChart: [],
-  //   videos: [],
-  sizes: [],
-  description: `
-    <div>
-        <div>
-            <h1><strong>PASTELLE</strong></h1>
-            <p>HEAVY. ORGANIC. STREETWEAR. PORTUGAL.</p>
-            <p>
-                PASTELLE APPAREL IS BASED IN LISBOA - 
-                THE CAPITAL OF PORTUGAL AND A GROWING HUB OF CREATIVE INDIVIDUALS WHOM SHARE A PASSION FOR EXPRESSION
-                WITHOUT SACRIFICING GENUINITY. 
-            </p>    
-            <p>    
-                CAN THERE BE SUSTAINED CREATIVE EXPRESSION 
-                WITHOUT GREED AND WASTE? LET'S FIND OUT TOGETHER.
-            </p>
-            <p>
-                HEAVY 250g+ STREETWEAR PIECES PRODUCED FROM ORGANIC MATERIALS WITH A LOW FOOTPRINT.
-            </p>
+  z-index: 0;
 
-            <p>
-                PASTELLE APPAREL IS BASED IN LISBOA - 
-                THE CAPITAL OF PORTUGAL AND A GROWING HUB OF CREATIVE INDIVIDUALS WHOM SHARE A PASSION FOR EXPRESSION
-                WITHOUT SACRIFICING GENUINITY. 
-            </p>    
-            <p>    
-                CAN THERE BE SUSTAINED CREATIVE EXPRESSION 
-                WITHOUT GREED AND WASTE? LET'S FIND OUT TOGETHER.
-            </p>
-            <p>
-                HEAVY 250g+ STREETWEAR PIECES PRODUCED FROM ORGANIC MATERIALS WITH A LOW FOOTPRINT.
-            </p>
+  > div {
+    height: 75%;
+  }
+`
 
-            <p>
-                PASTELLE APPAREL IS BASED IN LISBOA - 
-                THE CAPITAL OF PORTUGAL AND A GROWING HUB OF CREATIVE INDIVIDUALS WHOM SHARE A PASSION FOR EXPRESSION
-                WITHOUT SACRIFICING GENUINITY. 
-            </p>    
-            <p>    
-                CAN THERE BE SUSTAINED CREATIVE EXPRESSION 
-                WITHOUT GREED AND WASTE? LET'S FIND OUT TOGETHER.
-            </p>
-            <p>
-                HEAVY 250g+ STREETWEAR PIECES PRODUCED FROM ORGANIC MATERIALS WITH A LOW FOOTPRINT.
-            </p>
-        </div>
-    </div>
-  `,
-  artistInfo: undefined,
-  shortDescription: 'HEAVY.ORGANIC. STREETWEAR.PORTUGAL',
-  noVideo: false,
-  noDescription: false,
-}
 export default function Home() {
-  const { collection } = useCurrentCollection()
+  const homepage = useQueryHomepage()
 
   const navigate = useNavigate()
 
@@ -94,77 +40,115 @@ export default function Home() {
   const { onChange } = useProductWebCarouselActions({
     startIndex: 0,
   })
-  const product = useMemo(
-    () =>
-      Object.assign(
-        {
-          id: collection?.products?.['voodoo']?.id as string,
-          skillMetadata: collection?.products?.['voodoo']?.skillMetadata,
-          headerLogo: collection?.products?.['voodoo']?.headerLogo,
-          navLogo: collection?.products?.['voodoo']?.navLogo,
-          logo: urlToSimpleGenericImageSrcSet(LOGO),
-          images: (collection?.products?.['voodoo']?.images || []) as FragmentProductImageFragment[],
-          videos: (collection?.products?.['voodoo']?.videos || []) as FragmentProductVideoFragment[],
-        },
-        baseProduct
-      ),
-    [collection?.products]
-  )
 
   const isMobile = useIsMobile()
   // Get common props for screens
   // lockStatus === null means ignore skill state
-  const commonProps = useGetCommonPropsFromProduct({ ...product, variant: null, isMobile, lockStatus: null })
+  const commonProps = useGetCommonPropsFromProduct({
+    ...homepage,
+    bgColor: homepage?.bgColor || 'red',
+    color: homepage?.bgColor || 'red',
+    altColor: homepage?.bgColor || 'red',
+    lockedImages: [],
+    sizeChart: [],
+    title: homepage?.title || 'PASTELLE',
+    handle: homepage?.handle || 'PASTELLE',
+    productType: '',
+    variant: null,
+    isMobile,
+    lockStatus: null,
+  })
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const navigateToCollection = useCallback(() => navigate(COLLECTION_PATHNAME), [])
 
-  if (!product.videos.length || !product.images.length) return null
+  if (!homepage?.images?.length || !homepage?.videos?.length) return null
 
   return (
     <>
       <SEO title="HEAVY STREETWEAR" name="HEAVY STREETWEAR" description="PASTELLE. HEAVY STREETWEAR. PORTUGAL." />
-      <SingleAsidePage bgColor={product.bgColor} logo={product.logo} altLogo={product.navLogo}>
-        {({ rootContainerNode, screensContainerNode }) => (
+      <SinglePageSmartWrapper>
+        {({
+          screensContainerNode,
+          rootContainerNode,
+          asideContainerRef,
+          setScreensContainerRef,
+          parentAspectRatio,
+        }) => (
           <>
-            <Screens.AsideCarousel
-              {...commonProps}
-              containerNode={screensContainerNode}
-              carousel={{
-                images: product.images,
-                lockedImages: product.lockedImages,
-                videos: product.videos,
-                startIndex: 0,
-                onChange,
-                onCarouselItemClick: alert,
-              }}
-              themeMode={mode}
-              breadcrumbs={{
-                breadcrumbs: ['Home'],
-                lastCrumb: 'Home',
-              }}
-              skillInfo={null}
-              userAddress={undefined}
-            />
-            <Screens.ActionScreen
-              {...commonProps}
-              labels={{
-                main: 'VIEW COLLECTION',
-                async: 'HEADING TO COLLECTION',
-              }}
-              skillInfo={null}
-              callback={navigateToCollection}
-              fixedWidth={screensContainerNode?.clientWidth}
-              rootNode={rootContainerNode}
-            />
-            <Screens.Description
-              {...commonProps}
-              containerNode={screensContainerNode}
-              description={product.description}
-            />
+            <SingleProductContainer id="#item-container" parentAspectRatio={parentAspectRatio}>
+              <SingleProductAsidePanel id="#item-aside-panel" ref={asideContainerRef}>
+                <SingleProductScreensContainer
+                  $calculatedSizes={{
+                    height: asideContainerRef.current?.clientHeight,
+                    width: screensContainerNode?.clientWidth,
+                  }}
+                  ref={setScreensContainerRef}
+                  bgColor={homepage?.bgColor}
+                  navLogo={homepage?.navLogo}
+                  logo={homepage?.logo}
+                >
+                  <Screens.AsideCarousel
+                    {...commonProps}
+                    containerNode={screensContainerNode}
+                    carousel={{
+                      touchAction: 'pan-y',
+                      images: homepage.images,
+                      lockedImages: [],
+                      videos: [],
+                      startIndex: 0,
+                      onChange,
+                      onCarouselItemClick: alert,
+                      indicatorOptions: {
+                        showIndicators: true,
+                      },
+                    }}
+                    themeMode={mode}
+                    breadcrumbs={null}
+                    skillInfo={null}
+                    userAddress={undefined}
+                    logoCss="filter: unset;"
+                  />
+                  <Screens.ActionScreen
+                    {...commonProps}
+                    labels={{
+                      main: 'VIEW COLLECTION',
+                      async: 'HEADING TO COLLECTION',
+                    }}
+                    skillInfo={null}
+                    callback={navigateToCollection}
+                    fixedWidth={screensContainerNode?.clientWidth}
+                    rootNode={rootContainerNode}
+                  >
+                    <br />
+                  </Screens.ActionScreen>
+                  <Screens.Description
+                    {...commonProps}
+                    header="WHO ARE WE?"
+                    containerNode={screensContainerNode}
+                    description={homepage.description}
+                  />
+                </SingleProductScreensContainer>
+              </SingleProductAsidePanel>
+              <SelectedShowcaseVideo
+                forceLoad
+                firstPaintOver
+                isMobileWidth={false}
+                smartFill={{
+                  full: true,
+                }}
+                hideVideo={false}
+                css={VIDEO_CSS}
+                currentCarouselIndex={0}
+                selectedVideo={homepage.videos[0]}
+                videoProps={{
+                  loop: false,
+                }}
+              />
+            </SingleProductContainer>
           </>
         )}
-      </SingleAsidePage>
+      </SinglePageSmartWrapper>
     </>
   )
 }
