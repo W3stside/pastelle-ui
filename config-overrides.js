9e5e64355286e554898ea1a30bdf725c9fb0bfa1
virgil-pastelle <doohhello@gmail.com>
2023-10-19 22:03:47 +0100

    remove craco and client side csp

--- config-overrides.js
+++ config-overrides.js
@@ -2,73 +2,6 @@
 // @ts-check
 const webpack = require('webpack')
 const { override } = require('customize-cra')
-const cspHtmlWebpackPlugin = require('csp-html-webpack-plugin')
-
-const cspConfigPolicy = {
-  'default-src': "'none'",
-  'media-src': [
-    'https://cdn.shopify.com',
-    'https://ik.imagekit.io/pastelle/',
-    // hosting server
-    'https://d33wubrfki0l68.cloudfront.net',
-  ],
-  'img-src': [
-    "'self'",
-    // hosting server
-    'https://d33wubrfki0l68.cloudfront.net',
-    'https://ik.imagekit.io/pastelle/',
-    'https://cdn.shopify.com',
-    'https://cryptologos.cc/logos/versions/ethereum-eth-logo-colored.svg',
-    'https://images.web3auth.io',
-    'https://web3auth.io/images/',
-    'https://user-images.githubusercontent.com',
-    'https://raw.githubusercontent.com/WalletConnect/walletconnect-assets/',
-    'https://explorer-api.walletconnect.com',
-    'data:',
-  ],
-  'base-uri': "'self'",
-  'object-src': "'none'",
-  'script-src': [
-    "'self'",
-    // hosting server
-    'https://d33wubrfki0l68.cloudfront.net',
-    // google tag
-    "'sha256-jwsanj4WKKtrMM+7Khy+zjXFPYb+YaRZ/mLaS40iTgE='",
-  ],
-  'script-src-elem': [
-    "'self'",
-    // hosting server
-    'https://d33wubrfki0l68.cloudfront.net',
-    'https://www.googletagmanager.com',
-    // google tag
-    "'sha256-jwsanj4WKKtrMM+7Khy+zjXFPYb+YaRZ/mLaS40iTgE='",
-  ],
-  'connect-src': [
-    "'self'",
-    // hosting server
-    'https://d33wubrfki0l68.cloudfront.net',
-    'https://*.google-analytics.com',
-    // ipfs
-    'https://pastelle.infura-ipfs.io',
-    'https://ipfs.io/ipfs/',
-    // w3a
-    'wss://broadcast-server.tor.us',
-    'https://broadcast-server.tor.us',
-    'https://admin.openlogin.com/api/',
-    // wc
-    'https://explorer-api.walletconnect.com',
-    'wss://relay.walletconnect.com',
-    'https://rpc.walletconnect.com',
-    'https://e3f9fe-2.myshopify.com',
-    'https://polygon-rpc.com',
-    'https://rpc.ankr.com',
-    'https://pstlcollections.s3.eu-south-2.amazonaws.com',
-  ],
-  'manifest-src': "'self'",
-  'frame-src': ['https://verify.walletconnect.com', 'https://verify.walletconnect.org'],
-  'font-src': ["'self'", 'https://fonts.gstatic.com'],
-  'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-}
 
 function addWebpackOverride(config) {
   const fallback = config.resolve.fallback || {}
@@ -102,17 +35,9 @@ function addWebpackOverride(config) {
   return config
 }
 
-function addCspHtmlWebpackPlugin(config) {
-  if (process.env.NODE_ENV === 'development') {
-    config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy))
-  }
-
-  return config
-}
-
 module.exports = {
   babel: {
     plugins: ['@babel/plugin-proposal-nullish-coalescing-operator'],
   },
-  webpack: override(addWebpackOverride, addCspHtmlWebpackPlugin),
+  webpack: override(addWebpackOverride),
 }
