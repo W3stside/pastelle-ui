1d7041262d38592cec31b72014f4e88934f7ea11
virgil-pastelle <doohhello@gmail.com>
2024-09-29 18:14:48 +0100

    chore: increase build time next

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,8 @@ const nextConfig = {
   },
   reactStrictMode: false,
   // netlify build timing out @ 60 so increased here
-  staticPageGenerationTimeout: 240,
+  // 10 minutes
+  staticPageGenerationTimeout: 10 * 60,
   transpilePackages: ["@past3lle/web3-modal", "@past3lle/forge-web3", "@past3lle/wagmi-connectors"]
 }
  
